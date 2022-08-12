16fc0576ecc2716876578bf300766774e1cf27dc
r-shirasu <risako.shirasu@asia-quest.jp>
2022-08-12 17:36:02 +0900

    create: main index routing

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\TodoCreate;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +27,5 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::get("/todos/create", TodoCreate::class)->name("todos.create");
