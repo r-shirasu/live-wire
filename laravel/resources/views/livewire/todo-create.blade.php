9bf49f4842ef9229bcad6a89a43a9349ae50dd82
r-shirasu <risako.shirasu@asia-quest.jp>
2022-08-15 12:27:34 +0900

    start styling with tailwindcss

--- laravel/resources/views/livewire/todo-create.blade.php
+++ laravel/resources/views/livewire/todo-create.blade.php
@@ -1,13 +1,24 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
 <div>
     <form wire:submit.prevent="save">
         <div>
             <input type="text" wire:model="title">
         </div>
-
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
         <div>
             <textarea wire:model="content"></textarea>
         </div>
 
+
+
         <button type="submit">保存</button>
     </form>
 </div>
+
