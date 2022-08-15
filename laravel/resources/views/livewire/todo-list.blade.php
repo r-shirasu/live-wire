8fa1ac5e74942ffa8a03f4769ecd0dc9551c7e7c
r-shirasu <risako.shirasu@asia-quest.jp>
2022-08-15 15:47:29 +0900

    styled todolists show page

--- laravel/resources/views/livewire/todo-list.blade.php
+++ laravel/resources/views/livewire/todo-list.blade.php
@@ -1,12 +1,33 @@
-<div>
-    <ul>
-        @foreach($todos as $todo)
-            <li wire:key="{{ $todo->id }}">
-                <a href="{{ route('todos.show', ['todo' => $todo->id]) }}">{{ $todo->title }}</a>
-                <!-- 削除ボタンを追加 -->
-                <button wire:click="delete({{ $todo->id }})">削除</button>
-            </li>
-        @endforeach
-    </ul>
-    <a href="{{ route('todos.create') }}">作成</a>
+<div class="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
+    <div class="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
+        <div class="flex justify-between mb-5">
+            <h1 class="text-3xl font-bold">Todo List</h1>
+            <button
+                class="flex-no-shrink p-2 border-2 rounded text-blue-400 border-blue-400 hover:text-white hover:bg-blue-400"
+                onclick=location.href="{{ route('todos.create') }}">NewTodo Create
+            </button>
+        </div>
+        <div class="mt-5">
+            <ul>
+                @foreach($todos as $todo)
+                    <li class="flex justify-between" wire:key="{{ $todo->id }}">
+                        <div>
+                            <a>{{ $todo->title }}</a>
+                            <a>{{ $todo->content }}</a>
+                        </div>
+                        <div>
+                            <button
+                                class="justify-end p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-teal-400"
+                                onclick=location.href="{{ route('todos.show', ['todo' => $todo->id]) }}">Edit
+                            </button>
+                            <button
+                                class="justify-end p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red-400"
+                                wire:click="delete({{ $todo->id }})">Remove
+                            </button>
+                        </div>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
 </div>
